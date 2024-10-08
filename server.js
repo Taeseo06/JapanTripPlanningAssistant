6005447a0d0f3d945ffafc2396828983d99bfc66
Tae Seo Moon / 문태서 <ansxotj06@naver.com>
2024-10-08 14:10:42 +0900

    firebas-config 변경 전 배포
    firebase 설정을 .env 파일을 이용해서 불러오지 않고, index.html, server.js 에 지정해서 설정값을 가져오도록 변경

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import path from 'path';
 
+
 // 환경 변수 로드
 dotenv.config();
 
@@ -10,11 +11,18 @@ const app = express();
 const port = 3000;
 
 // CORS 허용 설정
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: '*', // 모든 도메인 허용
+    methods: ['GET', 'POST'], // 허용할 메소드
+}));
+
 
 // 정적 파일 제공을 위해 public 디렉토리 설정
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, 'public')));
+// const __dirname = path.resolve();
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 // Firebase 설정 정보 제공 엔드포인트
 app.get('/firebase-config', (req, res) => {
@@ -26,6 +34,8 @@ app.get('/firebase-config', (req, res) => {
         messagingSenderId: process.env.MESSAGING_SENDER_ID,
         appId: process.env.APP_ID,
     };
+
+    console.log("Firebase Config Requested:", firebaseConfig); // 로그 추가 - fibaseConfig 페이지 응답 확인
 
     if (firebaseConfig.apiKey) {
         res.json(firebaseConfig);
